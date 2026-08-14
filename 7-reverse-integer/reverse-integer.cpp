e6b61dc346cb70cbb57a45cee01fdb2518a1c3a3
aditibhatt-18 <aditibhatt1809@gmail.com>
2026-08-14 11:42:34 +0530

    Time: 5 ms (20.09%) | Memory: 8.3 MB (98.36%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -6,12 +6,12 @@ public:
             rem = x%10;
             x /=10 ;
 
-            if (ans>INT_MAX/10 || (ans == INT_MAX && rem >7)) return 0;
-            if(ans<INT_MIN/10 ||(ans == INT_MIN && rem <-8)) return 0;
+            if (ans>INT_MAX/10 || (ans == INT_MAX/10 && rem >7)) return 0;
+            if(ans<INT_MIN/10 ||(ans == INT_MIN/10 && rem <-8)) return 0;
 
             ans = ans*10 + rem;
         }
-        
+
         return ans;
         
     }
