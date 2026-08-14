7ea611f4bf5c14d9cabe06e7ddf165122cf3d78e
aditibhatt-18 <aditibhatt1809@gmail.com>
2026-08-14 12:56:35 +0530

    Time: 42 ms (44.57%) | Memory: 105.8 MB (64.14%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int left = 0,right = 0 , res = 0;
+        long long total =0;
+        int n = nums.size();
+        while(right<n){
+            total += nums[right];
+            
+            while((long long)nums[right] *(right-left+1) > total+k){
+                total -= nums[left];
+                left++;
+            }
+            res = max(res,right-left+1);
+            right++;
+
+        }
+        return res; 
+    }
+};
